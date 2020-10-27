a8b94fe3464b85360d9bb29d42892aa3b5c963c2
Lewis Munene <lewis.munene@lanstar.co.ke>
2020-10-27 18:04:40 +0300

    pass

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\LocationController;
-use App\Http\Controllers\PaymentsContoller;
+use App\Http\Controllers\PaymentsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +27,6 @@ Route::get('/wards', [LocationController::class, 'wards']);
 
 
 
-Route::get('/payment_result', [PaymentsContoller::class, 'index']);
+Route::get('/payment_result', [PaymentsController::class, 'index']);
 
-Route::post('/payment_result', [PaymentsContoller::class, 'index']);
+Route::post('/payment_result', [PaymentsController::class, 'index']);
